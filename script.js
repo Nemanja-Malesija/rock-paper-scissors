0434510e24da26144ee4c40c03bb0ec218e756b3
Nemanja Malesija <malesija13@gmail.com>
2022-06-23 07:53:54 +0200

    add game mechanics, work on overlay and winner display animation

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// selectors
+const container = document.querySelector('.container');
+const message = document.querySelector('.message');
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const allButtons = document.querySelectorAll('.btn');
+
+const overlay = document.querySelector('.overlay');
+const playerDisplay = document.querySelector('.div--player');
+const computerDisplay = document.querySelector('.div--computer');
+
 const choice = ['rock', 'paper', 'scissors'];
 
 // global variables
@@ -21,7 +33,7 @@ const playRound = function (playerSelection, computerSelection) {
     (playerSelection === 'paper' && computerSelection === 'paper') ||
     (playerSelection === 'scissors' && computerSelection === 'scissors')
   )
-    console.log('Draw');
+    message.textContent = 'DRAW';
   // test for player win
   else if (
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
@@ -29,7 +41,7 @@ const playRound = function (playerSelection, computerSelection) {
     (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
     playerScore++;
-    console.log('Player Wins');
+    message.textContent = 'PLAYER WINS';
 
     // test for computer win
   } else if (
@@ -38,10 +50,48 @@ const playRound = function (playerSelection, computerSelection) {
     (playerSelection === 'scissors' && computerSelection === 'rock')
   ) {
     computerScore++;
-    console.log('Computer Wins');
+    message.textContent = 'COMPUTER WINS';
   }
 };
 
+/* Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+Add a div for displaying results and change all of your console.logs into DOM methods.
+Display the running score, and announce a winner of the game once one player reaches 5 points. */
+
+// select button elements
+
+// run add event listener on a div
+container.addEventListener('click', function (e) {
+  if (e.target.classList.contains('btn')) {
+    const clicked = e.target.dataset.choice;
+    playRound(clicked, computerPlay());
+  }
+
+  // player wins
+  if (computerScore === 5) {
+    message.textContent = 'COMPUTER WINS THE GAME';
+    computerDisplay.classList.add('winner');
+    overlay.classList.remove('hidden');
+
+    // computer wins
+  } else if (playerScore === 5) {
+    message.textContent = 'YOU WIN THE GAME';
+    playerDisplay.classList.add('winner');
+    overlay.classList.remove('hidden');
+  }
+});
+
+// take e.target attribute
+
+// run playRound with (clicked, computer)
+
+// 1. add event listeners to buttons.
+
+// when button clicked, playRound
+
+// selection from round
+
+/*
 const game = function () {
   for (let i = 0; i < 5; i++) {
     computerChoice = computerPlay();
@@ -59,3 +109,4 @@ const game = function () {
 };
 
 game();
+*/
